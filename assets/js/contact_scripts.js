eb0e724f1003995afd3ce6cbca02e74cca4fb2ba
oi7 <oi7@mit.edu>
2016-02-25 20:42:16 -0500

    Updated position and zooming for google maps in contact page

--- assets/js/contact_scripts.js
+++ assets/js/contact_scripts.js
@@ -19,9 +19,9 @@ $(document).ready(function(){
     function initialize_map() {
 
         if ($('.map').length) {
-            var myLatLng = new google.maps.LatLng(39.987686, -82.970916);
+            var myLatLng = new google.maps.LatLng(42.3606587, -71.0896589);
             var mapOptions = {
-                zoom: 7,
+                zoom: 14,
                 center: myLatLng,
                 scrollwheel: false,
                 panControl: false,
